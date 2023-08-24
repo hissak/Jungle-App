85a52c6d19715f55b80c7580c566d4ec4c832c9f
vagrant <hassan.a.issak@gmail.com>
2023-08-24 12:59:12 +0000

    Add html for /signup view and add loging to Users#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    if user.save
+      flash[:notice] = "You have successfully signed up!"
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem signing up."
+      redirect_to :back
+    end
+    private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      end
   end
 
   def new
