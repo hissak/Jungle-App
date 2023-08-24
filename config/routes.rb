85d12452fab35aa71546a00510e750d0f187f4e5
vagrant <hassan.a.issak@gmail.com>
2023-08-24 12:43:51 +0000

    Create User table and Users route

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
 
   get '/about', to: 'about#index'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
