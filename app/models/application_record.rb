55d41f6cbbde8fc9d47c65abc435f2cc00191fcd
vagrant <hassan.a.issak@gmail.com>
2023-08-24 16:40:46 +0000

    Remove logic for soldout from product index view and replace with product.soldout?

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def soldout?
+    quantity == 0
+  end
+
+  def logged_in?
+    session[:user_id].present?
+  end
+  
 end
