a8fe36b1e0064ebe80a0b015e29a92161a1a06aa
vagrant <hassan.a.issak@gmail.com>
2023-08-24 07:42:50 +0000

    Create new.html.erb for admin/categories that allows admins to add a new category to database

--- spec/requests/admin/categories_spec.rb
+++ spec/requests/admin/categories_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Admin::Categories", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns http success" do
+      get "/admin/categories/index"
+      expect(response).to have_http_status(:success)
+    end
   end
+
 end
