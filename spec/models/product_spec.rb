a1e19ca23e154259384e69c183676b0f425194cf
vagrant <hassan.a.issak@gmail.com>
2023-08-25 08:06:21 +0000

    Add tests to confirm that products have categories when added

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,7 +18,19 @@ RSpec.describe Product, type: :model do
         @product = Product.new(name: 'Cats', price: 60, quantity: 4, category: @category)
         expect(@product.quantity).to be_present
       end
+      it ' should have a category' do
+        @category = Category.new(name: 'Bananas')
+        @product = Product.new(name: 'Cats', price: 60, quantity: 4, category: @category)
+        expect(@product.category).to be_present
+      end
   end
+    context 'when a field is missing' do
+      it ' should not have a name' do
+        @category = Category.new(name: 'Bananas')
+        @product = Product.new(name: nil, price: 60, quantity: 4, category: @category)
+        expect(@product.name).to be_nil
+      end
+    end
 end
 end
 
