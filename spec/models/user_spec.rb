d85bfb8472e66689068429289d4705c8ba28776a
vagrant <hassan.a.issak@gmail.com>
2023-08-25 10:13:15 +0000

    Add tests to confirm all user fields are required

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe User, type: :model do
         @user = User.new(first_name: 'Obi', last_name: 'Wan', email: '123@gmail.com', password: 'password', password_confirmation: 'password')
         expect(@user.save).to be_truthy
       end 
-      it 'should not save if email is not unique' do
+      it 'should not save if email is not unique, regardless of letter casing' do
         @user1 = User.new(first_name: 'Obi', last_name: 'Wan', email: '123@gmail.com', password: 'password', password_confirmation: 'password')
-        @user2 = User.new(first_name: 'Anakin', last_name: 'Skywalker', email: '123@gmail.com', password: 'password', password_confirmation: 'password')
+        @user2 = User.new(first_name: 'Anakin', last_name: 'Skywalker', email: '123@GMAIL.com', password: 'password', password_confirmation: 'password')
         expect(@user1.save).to be_truthy
         expect(@user2.save).to be_falsey
       end
@@ -17,6 +17,14 @@ RSpec.describe User, type: :model do
         @user = User.new(first_name: 'Obi', last_name: 'Wan', email: '123@gmail.com', password: 'password', password_confirmation: 'xyz')
         expect(@user.save).to be_falsey
       end
+      it 'should not save if email, first name or last name are missing' do
+        @user1 = User.new(first_name: 'Obi', last_name: 'Wan', email: nil, password: 'password', password_confirmation: 'password')
+        expect(@user1.save).to be_falsey
+        @user2 = User.new(first_name: nil, last_name: 'Wan', email: '123@gmail.com', password: 'password', password_confirmation: 'password')
+        expect(@user2.save).to be_falsey
+        @user3 = User.new(first_name: 'Obi', last_name: nil, email: '123@gmail.com', password: 'password', password_confirmation: 'password')
+        expect(@user3.save).to be_falsey
+        end
     end
   end
 end
