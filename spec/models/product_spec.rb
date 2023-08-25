abf5b8c264db0e10075501d69135d9b39413a6b7
vagrant <hassan.a.issak@gmail.com>
2023-08-25 08:35:48 +0000

    Change variable name from price to price_cents

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,30 +5,30 @@ RSpec.describe Product, type: :model do
     context 'when all fields are provided' do
       it 'should save successfully' do
         @category = Category.new(name: 'Bananas')
-        @product = Product.new(name: 'Cats', price: 60, quantity: 4, category: @category)
+        @product = Product.new(name: 'Cats', price_cents: 60, quantity: 4, category: @category)
         expect(@product.save).to be_truthy
       end
       it 'should have a name' do
         @category = Category.new(name: 'Bananas')
-        @product = Product.new(name: 'Cats', price: 60, quantity: 4, category: @category)
+        @product = Product.new(name: 'Cats', price_cents: 60, quantity: 4, category: @category)
         @product.save
         expect(@product.name).to be_present
       end
       it 'should have a price' do
         @category = Category.new(name: 'Bananas')
-        @product = Product.new(name: 'Cats', price: 60, quantity: 4, category: @category)
+        @product = Product.new(name: 'Cats', price_cents: 60, quantity: 4, category: @category)
         @product.save
         expect(@product.price).to be_present
       end
       it 'should have a quantity' do
         @category = Category.new(name: 'Bananas')
-        @product = Product.new(name: 'Cats', price: 60, quantity: 4, category: @category)
+        @product = Product.new(name: 'Cats', price_cents: 60, quantity: 4, category: @category)
         @product.save
         expect(@product.quantity).to be_present
       end
       it 'should have a category' do
         @category = Category.new(name: 'Bananas')
-        @product = Product.new(name: 'Cats', price: 60, quantity: 4, category: @category)
+        @product = Product.new(name: 'Cats', price_cents: 60, quantity: 4, category: @category)
         @product.save
         expect(@product.category).to be_present
       end
@@ -36,10 +36,10 @@ RSpec.describe Product, type: :model do
     context 'when a field is missing' do
       it 'should not save successfully' do
         @category = Category.new(name: 'Bananas')
-        @product1 = Product.new(name: nil, price: 60, quantity: 4, category: @category)
-        @product2 = Product.new(name: 'Cats', quantity: 4, category: @category)
-        @product3 = Product.new(name: 'Cats', price: 60, quantity: nil, category: @category)
-        @product4 = Product.new(name: 'Cats', price: 60, quantity: 4, category: nil)
+        @product1 = Product.new(name: nil, price_cents: 60, quantity: 4, category: @category)
+        @product2 = Product.new(name: 'Cats', price_cents: nil, quantity: 4, category: @category)
+        @product3 = Product.new(name: 'Cats', price_cents: 60, quantity: nil, category: @category)
+        @product4 = Product.new(name: 'Cats', price_cents: 60, quantity: 4, category: nil)
 
         expect(@product1.save).to be_falsey
         expect(@product2.save).to be_falsey
