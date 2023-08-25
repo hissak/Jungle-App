5f65481e60f1067a4a7158724f7ea72bb4bc1b6f
vagrant <hassan.a.issak@gmail.com>
2023-08-25 10:08:26 +0000

    Add test to confirm users cannot register with an email that already exists and password and password_confirmation must match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe User, type: :model do
         @user = User.new(first_name: 'Obi', last_name: 'Wan', email: '123@gmail.com', password: 'password', password_confirmation: 'password')
         expect(@user.save).to be_truthy
       end 
+      it 'should not save if email is not unique' do
+        @user1 = User.new(first_name: 'Obi', last_name: 'Wan', email: '123@gmail.com', password: 'password', password_confirmation: 'password')
+        @user2 = User.new(first_name: 'Anakin', last_name: 'Skywalker', email: '123@gmail.com', password: 'password', password_confirmation: 'password')
+        expect(@user1.save).to be_truthy
+        expect(@user2.save).to be_falsey
+      end
+      it 'should not save if password and password_confirmation do not match' do
+        @user = User.new(first_name: 'Obi', last_name: 'Wan', email: '123@gmail.com', password: 'password', password_confirmation: 'xyz')
+        expect(@user.save).to be_falsey
+      end
     end
   end
 end
